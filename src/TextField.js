2f42b001f56c71003b5957c54e60d9000b02a4fa
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-11-03 12:50:43 -0400

    add basic styling

--- src/TextField.js
+++ src/TextField.js
@@ -4,31 +4,36 @@ import { If, Then, Else } from 'react-if'
 class TextField extends Component {
   render() {
     return (
-      <If condition={this.props.enabled} >
-        <Then>
-          <div className={this.props.className}>
-            <textarea
-              placeholder={this.props.placeholder}
-              defaultValue={this.props.defaultValue}
-              value={this.props.value}
-              onChange={this.props.onChange}
-            />
-            <button
-              onClick={this.props.onClick}
-            >
-              Next
-            </button>
-          </div>
-        </Then>
-        <Else>
-          <div className={this.props.className}>
-            <textarea disabled
-              value={ this.props.value || this.props.text }
-            ></textarea>
-            <button disabled>Next</button>
-          </div>
-        </Else>
-      </If>
+        <If condition={this.props.enabled} >
+          <Then>
+            <div className={this.props.className}>
+            <p className="stage-heading">{this.props.heading }</p>
+              <textarea
+                className="passage"
+                placeholder={this.props.placeholder}
+                defaultValue={this.props.defaultValue}
+                value={this.props.value}
+                onChange={this.props.onChange}
+              />
+              <button
+                onClick={this.props.onClick}
+              >
+                Next
+              </button>
+            </div>
+          </Then>
+          <Else>
+            <div className={this.props.className}>
+              <p className="stage-heading">{this.props.heading}</p>
+              <textarea
+                disabled
+                className="passage"
+                value={ this.props.value || this.props.text }
+              ></textarea>
+              <button disabled>Next</button>
+            </div>
+          </Else>
+        </If>
     )
   }
 }
