7302fe6b371939b7ea325dbcb074396ed92e21d1
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-11-03 10:43:14 -0400

    stage one form functional

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 import React, { Component } from 'react';
+import { If, Then, Else } from 'react-if'
+
 const logo = '//d2t498vi8pate3.cloudfront.net/assets/home-header-logo-8d37f4195730352f0055d39f7e88df602e2d67bdab1000ac5886c5a492400c9d.png';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      correctPassage: '',
+      errorPassage: '',
+      errors: [],
+      stageOneEnabled: true,
+      stageOneVisible: true,
+      stageTwoEnabled: false,
+      stageTwoVisible: false,
+      stageThreeEnabled: false,
+      stageThreeVisible: false
+    }
+
+    this.handleCorrectPassageChange = this.handleCorrectPassageChange.bind(this)
+    this.handleCorrectPassageSubmit = this.handleCorrectPassageSubmit.bind(this)
+
+  }
+
+  handleCorrectPassageChange(event) {
+    this.setState({ correctPassage: event.target.value })
+  }
+
+  handleCorrectPassageSubmit(event) {
+    this.setState({
+      stageOneEnabled: false,
+      stageTwoEnabled: true,
+      stageTwoEnabled: true
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,13 +44,39 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to the Quill Web Dev Challenge</h2>
         </div>
-        <p className="App-intro">
-          Refer to the <a href="https://github.com/empirical-org/quill-web-dev-challenge" target="_blank">README</a> for the instructions to this challenge.
-          <br/>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="interface">
+          <If condition={ this.state.stageOneVisible }>
+            <Then>
+              <If condition={ this.state.stageOneEnabled }>
+                <Then>
+                  <div className="stageOne">
+                    <textarea
+                      placeholder="Write passage here..."
+                      value={ this.state.correctPassage }
+                      onChange={ this.handleCorrectPassageChange }
+                    ></textarea>
+                    <button
+                      onClick={ this.handleCorrectPassageSubmit }
+                    >
+                      Next
+                    </button>
+                  </div>
+                </Then>
+              <Else>
+                <div className="stageOne">
+                  <textarea disabled
+                    value={ this.state.correctPassage }
+                  ></textarea>
+                  <button disabled>Next</button>
+                </div>
+              </Else>
+              </If>
+            </Then>
+          </If>
+        </div>
       </div>
-    );
+    )
   }
 }
 
